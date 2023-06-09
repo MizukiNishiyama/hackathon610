6db76e7349a3af1edc0220a6d87703ba4e73d32f
MizukiNishiyama <125176828+MizukiNishiyama@users.noreply.github.com>
2023-06-09 15:20:52 +0900

    15:20

--- hackathon/dao/message_dao.go
+++ hackathon/dao/message_dao.go
@@ -39,6 +39,6 @@ func (dao *MessageDao) DeleteMessage(id string) error {
 }
 
 func (dao *MessageDao) EditMessage(id, content string) error {
-	_, err := dao.DB.Exec("UPDATE message SET message_content = ? WHERE message_id = ?", content, id)
+	_, err := dao.DB.Exec("UPDATE message SET message_content = CONCAT(message_content, ' 編集済み') WHERE message_id = ?", id)
 	return err
 }
